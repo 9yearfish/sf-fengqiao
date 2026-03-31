bebc2cbd54c71ef1f62edbaf4f247abbee10b86c
aoxiang <makenmail@qq.com>
2026-03-31 19:47:15 +0800

    feat: sync client updates

--- src/Client.php
+++ src/Client.php
@@ -5,8 +5,6 @@ namespace Aoxiang\FengQiao;
 use Aoxiang\FengQiao\Requests\CargoDetails;
 use Aoxiang\FengQiao\Requests\Customer;
 use Aoxiang\FengQiao\Requests\Route;
-use phpDocumentor\Reflection\Types\Boolean;
-
 class Client
 {
     protected $partnerId;
@@ -16,7 +14,7 @@ class Client
     protected $debug = false;
     protected $result = null;
     //沙箱环境的地址
-    protected $sandBoxUrl = "http://sfapi-sbox.sf-express.com/std/service";
+    protected $sandBoxUrl = "https://sfapi-sbox.sf-express.com/std/service";
     //生产环境的地址
     protected $url = "https://sfapi.sf-express.com/std/service";
 
@@ -43,15 +41,32 @@ class Client
      * @param  Customer      发件人 $sendCustomer
      * @param  Customer      收件人 $recoverCustomer
      */
-    public function createOrder($orderId, CargoDetails $cargoDetails, Customer $sendCustomer, Customer $recoverCustomer)
+    public function createOrder($orderId, CargoDetails $cargoDetails, Customer $sendCustomer, Customer $recoverCustomer, array $options = [])
     {
         $data = [
-            'orderId'         => $orderId,
-            'cargoDetails'    => $cargoDetails,
-            'contactInfoList' => [
-                $sendCustomer, $recoverCustomer,
-            ],
+            'orderId'           => $orderId,
+            'cargoDetails'      => [$cargoDetails],
+            'contactInfoList'   => [$sendCustomer, $recoverCustomer],
+            'language'          => 'zh_CN',
+            'isReturnWaybillNo' => 1,
         ];
+
+        if( isset($options['monthlyCard']) && $options['monthlyCard'] ){
+            $data['monthlyCard'] = $options['monthlyCard'];
+        }
+        if( isset($options['isDocall']) ){
+            $data['isDocall'] = $options['isDocall'] ? 1 : 0;
+        }
+        if( isset($options['payMethod']) ){
+            $data['payMethod'] = $options['payMethod'];
+        }
+        if( isset($options['expressTypeId']) ){
+            $data['expressTypeId'] = $options['expressTypeId'];
+        }
+        if( isset($options['remark']) ){
+            $data['remark'] = $options['remark'];
+        }
+
         $this->setServiceCode('EXP_RECE_CREATE_ORDER')->request($data);
 
         return $this;
@@ -76,11 +91,67 @@ class Client
 
 
     /**
+     * 云打印面单转PDF接口
+     * 返回面单的PDF Base64数据，可用于图片转换后打印
+     *
+     * @param  string  $waybillNo  运单号
+     * @param  string  $templateCode  模板编码，如 fm_150_standard
+     * @param  string  $version  版本号
+     * @return $this
+     * @throws FengQiaoException
+     */
+    public function getWaybillPdf(string $waybillNo, string $templateCode = 'fm_150_standard', string $version = '2.0', string $fileType = 'pdf')
+    {
+        $data = [
+            'templateCode' => $templateCode,
+            'version'      => $version,
+            'fileType'     => $fileType,
+            'sync'         => 1,
+            'documents'    => [
+                [
+                    'masterWaybillNo' => $waybillNo,
+                ],
+            ],
+        ];
+        $this->setServiceCode('COM_RECE_CLOUD_PRINT_WAYBILLS')->request($data);
+
+        return $this;
+    }
+
+    /**
+     * 云打印面单转指令接口（CPCL/ZPL）
+     * 返回打印机可直接执行的指令字符串
+     * 预留接口，待购入支持CPCL的面单打印机后启用
+     *
+     * @param  string  $waybillNo  运单号
+     * @param  string  $templateCode  模板编码，如 fm_150_standard_CPCL
+     * @param  string  $version  版本号
+     * @return $this
+     * @throws FengQiaoException
+     */
+    public function getWaybillCommand(string $waybillNo, string $templateCode = 'fm_150_standard_CPCL', string $version = '2.0')
+    {
+        $data = [
+            'templateCode' => $templateCode,
+            'version'      => $version,
+            'sync'         => 1,
+            'documents'    => [
+                [
+                    'masterWaybillNo' => $waybillNo,
+                ],
+            ],
+        ];
+        $this->setServiceCode('COM_RECE_CLOUD_PRINT_COMMAND')->request($data);
+
+        return $this;
+    }
+
+    /**
      * @return mixed
      */
     public function getContent()
     {
-        return $this->result->apiResultData->msgData;
+        return $this->result->apiResultData->msgData ?? null;
     }
 
     /**
@@ -123,52 +194,48 @@ class Client
      */
     protected function request($data)
     {
-        $data            = json_encode($data);
+        $data            = json_encode($data, JSON_UNESCAPED_UNICODE);
         $this->timestamp = time();
-        //发送参数
-        $post_data = array(
+        $post_data       = [
             'partnerID'   => $this->partnerId,
             'requestID'   => $this->createUuid(),
             'serviceCode' => $this->serviceCode,
             'timestamp'   => $this->timestamp,
             'msgDigest'   => $this->createDigest($data),
             'msgData'     => $data,
-        );
-
+        ];
 
         $postdata = http_build_query($post_data);
-        $options  = array(
-            'http' => array(
-                'method'  => 'POST',
-                'header'  => 'Content-type:application/x-www-form-urlencoded;charset=utf-8',
-                'content' => $postdata,
-                'timeout' => 15 * 60 // 超时时间（单位:s）
-            ),
-        );
-        $context  = stream_context_create($options);
-        if( $this->debug ){
-            $url = $this->sandBoxUrl;
-        } else {
-            $url = $this->url;
+        $url      = $this->debug ? $this->sandBoxUrl : $this->url;
+        $raw      = $this->post($url, $postdata);
+
+        \Log::info('顺丰API原始返回', ['raw' => mb_substr($raw, 0, 2000)]);
+
+        $result = json_decode($raw);
+        if( !is_object($result) ){
+            throw new FengQiaoException('数据解析失败: ' . substr($raw, 0, 200));
         }
-        $result = file_get_contents($url, false, $context);
-        $result = json_decode($result);
-        if( is_object($result) ){
-            $result->apiResultData = json_decode($result->apiResultData);
 
-            if( $result->apiResultData->success ){
-                $this->result = $result;
-
-                return $this->result;
-            } else {
-                throw new FengQiaoException($result->apiResultData->errorMsg);
+        if( isset($result->apiResultData) ){
+            if( is_string($result->apiResultData) ){
+                $result->apiResultData = json_decode($result->apiResultData);
             }
-
         } else {
-            throw new FengQiaoException('数据解析失败');
+            $errorMsg  = isset($result->apiErrorMsg) ? $result->apiErrorMsg : '未知错误';
+            $errorCode = isset($result->apiResultCode) ? $result->apiResultCode : '';
+            throw new FengQiaoException('顺丰API错误[' . $errorCode . ']: ' . $errorMsg);
         }
 
-        return $result;
+        if( isset($result->apiResultData->success) && $result->apiResultData->success ){
+            $this->result = $result;
+
+            return $this->result;
+        }
+
+        $errorMsg = isset($result->apiResultData->errorMsg) ? $result->apiResultData->errorMsg : '未知业务错误';
+        $errorCode = isset($result->apiResultData->errorCode) ? $result->apiResultData->errorCode : '';
+
+        throw new FengQiaoException($errorMsg . ($errorCode ? " (Code: {$errorCode})" : ''));
     }
 
     /**
@@ -179,6 +246,69 @@ class Client
     protected function createDigest($data)
     {
         return base64_encode(md5((urlencode($data . $this->timestamp . $this->checkWord)), true));
+    }
+
+    /**
+     * @param  string  $url
+     * @param  string  $postdata
+     *
+     * @return string
+     * @throws FengQiaoException
+     */
+    protected function post($url, $postdata)
+    {
+        if( function_exists('curl_init') ){
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Content-type:application/x-www-form-urlencoded;charset=utf-8',
+            ]);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            if( defined('CURL_IPRESOLVE_V4') ){
+                curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+            }
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+            $response = curl_exec($ch);
+            if( $response === false ){
+                $error = curl_error($ch);
+                $errno = curl_errno($ch);
+                curl_close($ch);
+                throw new FengQiaoException('网络请求失败: ' . $error . ($errno ? " (#{$errno})" : ''));
+            }
+            curl_close($ch);
+
+            return $response;
+        }
+
+        $options = [
+            'http' => [
+                'method'  => 'POST',
+                'header'  => 'Content-type:application/x-www-form-urlencoded;charset=utf-8',
+                'content' => $postdata,
+                'timeout' => 15 * 60,
+            ],
+            'ssl'  => [
+                'verify_peer'      => false,
+                'verify_peer_name' => false,
+            ],
+        ];
+        $context = stream_context_create($options);
+        $result  = @file_get_contents($url, false, $context);
+
+        if( $result === false ){
+            $error   = error_get_last();
+            $message = '网络请求失败';
+            if( !empty($error['message']) ){
+                $message .= '：' . $error['message'];
+            }
+            throw new FengQiaoException($message);
+        }
+
+        return $result;
     }
 
     /**
--- src/Requests/Customer.php
+++ src/Requests/Customer.php
@@ -1,41 +1,175 @@
 <?php
 
-
 namespace Aoxiang\FengQiao\Requests;
 
-
-class Customer
+/**
+ * 顺丰联系人信息
+ */
+class Customer implements \JsonSerializable
 {
-//{
-//        "address": "广东省广州市白云区湖北大厦",
-//      "company": "顺丰速运",
-//      "contact": "小邱",
-//      "contactType": 2,
-//      "country": "CN",
-//      "postCode": "580058",
-//      "tel": "18688806057"
-//    }
+    /**
+     * 联系人类型
+     * 1: 寄件人
+     * 2: 收件人
+     * @var int
+     */
+    public $contactType = 1;
 
-    public $address = "";
-
+    /**
+     * 联系人姓名
+     * @var string
+     */
     public $contact = "";
 
-    public $contactType = "";
+    /**
+     * 手机号码
+     * @var string
+     */
+    public $mobile = "";
 
+    /**
+     * 固定电话（可选）
+     * @var string
+     */
     public $tel = "";
 
+    /**
+     * 国家代码
+     * @var string
+     */
     public $country = "CN";
 
+    /**
+     * 省份
+     * @var string
+     */
+    public $province = "";
+
+    /**
+     * 城市
+     * @var string
+     */
+    public $city = "";
+
+    /**
+     * 区县
+     * @var string
+     */
+    public $county = "";
+
+    /**
+     * 详细地址
+     * @var string
+     */
+    public $address = "";
+
+    /**
+     * 邮编
+     * @var string
+     */
     public $postCode = "";
 
+    /**
+     * 公司名称
+     * @var string
+     */
     public $company = "";
 
-
-    public function __construct(string $contact, string $address, string $tel, $contactType = 1)
+    /**
+     * 兼容旧版构造函数
+     *
+     * @param string|null $contact 联系人姓名
+     * @param string|null $address 地址
+     * @param string|null $tel 电话
+     * @param int $contactType 联系人类型
+     */
+    public function __construct(?string $contact = null, ?string $address = null, ?string $tel = null, int $contactType = 1)
     {
-        $this->contact     = $contact;
-        $this->address     = $address;
-        $this->tel         = $tel;
+        if ($contact !== null) {
+            $this->contact = $contact;
+        }
+        if ($address !== null) {
+            $this->address = $address;
+        }
+        if ($tel !== null) {
+            $this->tel = $tel;
+            $this->mobile = $tel;
+        }
         $this->contactType = $contactType;
+    }
+
+    /**
+     * 设置联系人信息
+     *
+     * @param string $name 姓名
+     * @param string $mobile 手机号
+     * @return $this
+     */
+    public function setContact(string $name, string $mobile): self
+    {
+        $this->contact = $name;
+        $this->mobile = $mobile;
+        return $this;
+    }
+
+    /**
+     * 设置地址信息
+     *
+     * @param string $province 省份
+     * @param string $city 城市
+     * @param string $county 区县
+     * @param string $address 详细地址
+     * @return $this
+     */
+    public function setAddress(string $province, string $city, string $county, string $address): self
+    {
+        $this->province = $province;
+        $this->city = $city;
+        $this->county = $county;
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * 序列化为 JSON
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        $data = [
+            'contactType' => $this->contactType,
+            'contact'     => $this->contact,
+            'country'     => $this->country,
+            'address'     => $this->address,
+        ];
+
+        // 添加手机号或电话
+        if ($this->mobile) {
+            $data['mobile'] = $this->mobile;
+        }
+        if ($this->tel) {
+            $data['tel'] = $this->tel;
+        }
+
+        // 添加省市区（如果有）
+        if ($this->province) {
+            $data['province'] = $this->province;
+        }
+        if ($this->city) {
+            $data['city'] = $this->city;
+        }
+        if ($this->county) {
+            $data['county'] = $this->county;
+        }
+
+        // 可选字段
+        if ($this->postCode) {
+            $data['postCode'] = $this->postCode;
+        }
+        if ($this->company) {
+            $data['company'] = $this->company;
+        }
+
+        return $data;
     }
 }
